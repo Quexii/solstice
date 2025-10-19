c4edbcc6889d70a5f1aea153549c20919e4878d2
Shoroa_ <80775874+Quexii@users.noreply.github.com>
2025-10-19 21:10:42 +0200

    Revert "Add virtual asset support and restructure asset pipeline"

--- engine/src/main/kotlin/cc/lapiz/solstice/core/assets/meta/Meta.kt
+++ engine/src/main/kotlin/cc/lapiz/solstice/core/assets/meta/Meta.kt
@@ -1,21 +1,19 @@
 package cc.lapiz.solstice.core.assets.meta
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import java.nio.file.Path
 
 abstract class Meta<SerializeType>(val path: Path) {
     abstract val version: String
-    abstract val metaType: String
     var content: SerializeType? = null
     abstract fun onCreate(): String
     abstract fun onModify(original: String, force: Boolean): String
-    open fun loadFromDisk(serialized: String) {}
 
     @Serializable
     open class BaseSerialized(
         var version: String = "",
         var uid: String = "",
-        var path: String = "",
-        var metaType: String = "",
+        var path: String = ""
     )
 }
